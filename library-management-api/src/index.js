8dc2247f8b5fc9564da65abef398e1901fe44247
Affan <affanqaisar879@gmail.com>
2025-07-27 04:57:47 +0500

    set utils

--- library-management-api/src/index.js
+++ library-management-api/src/index.js
@@ -6,4 +6,18 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.error("Server error:", err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed!!! ", err);
+    process.exit(1);
+  });
