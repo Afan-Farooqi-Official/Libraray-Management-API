5d2e5f68ff76639f9e4ab5aef0c630926faa5671
Affan <affanqaisar879@gmail.com>
2025-08-01 17:01:26 +0500

    book Update

--- library-management-api/src/controllers/book.contoller.js
+++ library-management-api/src/controllers/book.contoller.js
@@ -1,3 +1,4 @@
+import { isValidObjectId } from "mongoose";
 import { Book } from "../models/book.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -22,12 +23,146 @@ const createBook = asyncHandler(async (req, res) => {
         .json(new ApiResponse(201, book, "book added successfully"));
 });
 
-const updateBook = asyncHandler(async (req, res) => {});
+const updateBook = asyncHandler(async (req, res) => {
+    //Extract book id, you want to update
+    const { bookId } = req.body;
+    const { title, author, genre } = req.body;
 
-const deleteBook = asyncHandler(async (req, res) => {});
+    //Check if book with this id exist
+    if (!bookId || !isValidObjectId(bookId)) {
+        throw new ApiError(400, "Invalid book ID");
+    }
 
-const getAllBooks = asyncHandler(async (req, res) => {});
+    //Check if user not give any info
+    if (!title || title.trim() === "") {
+        throw new ApiError(400, "Title is required");
+    }
+    if (!author || author.trim() === "") {
+        throw new ApiError(400, "Author is required");
+    }
+    if (!genre || genre.trim() === "") {
+        throw new ApiError(400, "Genre is required");
+    }
 
-const getBookById = asyncHandler(async (req, res) => {});
+    //Find and update
+    const book = await Book.findByIdAndUpdate(
+        bookId,
+        { title, author, genre },
+        { new: true, runValidators: true }
+    );
+    if (!book) {
+        throw new ApiError(404, "Book not found");
+    }
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Book updated successfully"));
+});
+
+const deleteBook = asyncHandler(async (req, res) => {
+    //Extract book id, you want to update
+    const { bookId } = req.body;
+
+    //Check if book with this id exist
+    if (!bookId || !isValidObjectId(bookId)) {
+        throw new ApiError(400, "Invalid book ID");
+    }
+
+    //Find and delete
+    const book = await Book.findByIdAndDelete(bookId);
+    if (!book) {
+        throw new ApiError(404, "Book not found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Book deleted successfully"));
+});
+
+const getAllBooks = asyncHandler(async (req, res) => {
+    //Get all books based on query, sort and pagination
+    const { page = 1, limit = 10, query, sortBy, sortType } = req.body;
+
+    //Validate pagination parameters, Ensure page an limit are numbers and greater than 0
+    const pageNumber = parseInt(page, 10);
+    const pageSize = parseInt(limit, 10);
+
+    //Validate page number and size
+    if (isNaN(pageNumber) || pageNumber < 1) {
+        throw new ApiError(400, "Invalid page number");
+    }
+    if (isNaN(pageSize) || pageSize < 1) {
+        throw new ApiError(400, "Invalid page size");
+    }
+
+    //Build query, this wil allow to search by title, author or genre
+    const queryObject = {};
+    if (query) {
+        queryObject.$or = [
+            {
+                title: { $regex: query, $options: "i" },
+            },
+            {
+                author: { $regex: query, $options: "i" },
+            },
+            {
+                genre: { $regex: query, $options: "i" },
+            },
+        ];
+    }
+
+    //Build sort object, If sortBy is provided, sort by the specified field
+    //Default to sorting by createdAt in descending order if no sortBy is provided
+    const sortObject = {};
+    if (sortBy) {
+        const sortField = sortBy === "createdAt" ? "createdAt" : "title";
+        sortObject[sortField] = sortType === "desc" ? -1 : 1;
+    } else {
+        sortObject.createdAt = -1; // Default to descending order by createdAt
+    }
+
+    // Fetch videos with pagination, sorting, and filtering
+    const books = await Book.find(queryObject)
+        .sort(sortObject)
+        .skip((pageNumber - 1) * pageSize)
+        .limit(pageSize)
+        .exec();
+    if (!books || books.length === 0) {
+        throw new ApiError(404, "No books found");
+    }
+
+    //Get total count of books for pagination
+    const totalCount = await Book.countDocuments(queryObject).exec();
+    const totalPages = Math.ceil(totalCount / pageSize);
+    const response = {
+        books,
+        pagination: {
+            totalCount,
+            totalPages,
+            currentPage: pageNumber,
+            pageSize,
+        },
+    };
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, response, "Books retrieved successfully"));
+});
+
+const getBookById = asyncHandler(async (req, res) => {
+    const { bookId } = req.body;
+
+    if (!bookId || !isValidObjectId(bookId)) {
+        throw new ApiError(400, "Invalid book ID");
+    }
+
+    const book = await Book.findById(bookId);
+    if (!book) {
+        throw new ApiError(404, "Book not found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, book, "Book retrieved successfully"));
+});
 
 export { createBook, updateBook, deleteBook, getAllBooks, getBookById };
--- library-management-api/src/routes/book.routes.js
+++ library-management-api/src/routes/book.routes.js
@@ -11,13 +11,13 @@ import { isAdmin, verifyJWT } from "../middlewares/auth.middleware.js";
 const route = Router();
 
 //Routes for all users
-route.get("/", getAllBooks);
-route.get("/:id", getBookById);
+route.post("/getAll-books", getAllBooks);
+route.get("/getById-book", getBookById);
 
 //Routes for admins only
 route.use(verifyJWT, isAdmin);
 route.post("/add-book", createBook);
-route.patch("/:id", updateBook); //update-book
-route.delete("/:id", deleteBook); //delete-book
+route.patch("/update-book", updateBook); //update-book
+route.delete("/delete-book", deleteBook); //delete-book
 
 export default route;
